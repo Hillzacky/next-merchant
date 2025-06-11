0f23ec37da8a3fcf63ee046c8c10f0f72b83302f
Hilmansyah <Hilm4nsyah@gmail.com>
2025-06-11 11:02:03 +0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>App</title>
+      </Head>
+
+      <h1>Welcome to my app!</h1>
+      <p>This is the index page.</p>
+    </div>
+  );
+}
