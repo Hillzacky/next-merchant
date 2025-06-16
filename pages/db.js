86d87e3682124d71834e150f91fe9528bf53ed26
Hilmansyah <Hilm4nsyah@gmail.com>
2025-06-16 11:23:47 +0700

    Update db.js

--- pages/db.js
+++ pages/db.js
@@ -1,4 +1,18 @@
+import postgres from 'postgres';
 
+const sql = postgres(process.env.DATABASE_URL,  { ssl: 'verify-full' });
+
+async function up() {
+  const m = await sql`
+    CREATE TABLE IF NOT EXISTS merchant(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      address TEXT NOT NULL,
+      phone VARCHAR(16) NULL
+    )
+  `
+  return m
+}
 
 async function getData(phone = null) {
   const m = await sql`
@@ -24,3 +38,5 @@ async function setData({ name, address, phone }) {
   `
   return m
 }
+
+module.exports = { up, getData, setData }
