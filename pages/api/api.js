0e6a1c58a622f389a331c8032a7030da4aa3c641
hillzacky <hilmansyah@xhr.my.id>
2025-06-17 05:10:28 +0700

    fix: perbaiki konfigurasi routing Express dan middleware

--- pages/api/api.js
+++ pages/api/api.js
@@ -5,8 +5,30 @@ import bulk from './bulk.js';
 import single from './single.js';
 import init from './init-db.js';
 
-app.use('/bulk', bulk);
-app.use('/single', single);
-app.use('/init-db', init);
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+// Routes
+app.use('/api/bulk', bulk);
+app.use('/api/single', single);
+app.use('/api/init-db', init);
+
+// Error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: err });
+});
 
 export default app;
